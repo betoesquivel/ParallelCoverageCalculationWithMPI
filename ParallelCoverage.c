496b4ca1a152b02256f096850aac61bf95f6e65b
Beto Esquivel <ppbeto94@gmail.com>
2015-12-10 20:18:03 +0000

    Correct formtatting of code.

--- ParallelCoverage.c
+++ ParallelCoverage.c
@@ -1,8 +1,17 @@
 //-----------------------------------------------------------------------------
 // c c o v e r a g e   --   calculate the coverage of feature detectors
 //-----------------------------------------------------------------------------
-// COMPILE: gcc -Wall -g -o ccoverage ccoverage.c -lm
+// COMPILE: mpicc -o coverage ParallelCoverage.c
+// RUN: mpiexec -machinefile machinefile -n 90 ./coverage
+//
+// In one node within an MPI cluster with open_mpi installed where:
+// - machinefile is the name of a file with all addresses of the nodes
+// in the cluster each in a new line
+// - 90 is the number of processes you wish to run in your cluster
 //-----------------------------------------------------------------------------
+// Base source code by: Dr. Adrian Clark
+// Reg no. of student: 1500921
+// Submission date: 10 December, 2015
 
 #include <stdio.h>
 #include <mpi.h>
@@ -13,25 +22,6 @@
 #define MAXFN   200
 #define MAXLINE 2000
 #define MAXLOCS 200000
-
-/*
- * MPI SYNTAX
- * MPI_Init(&argc, &argv);
- * int rank, size;
- * MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- * MPI_Comm_size(MPI_COMM_WORLD, &size);
- *
- * MPI_Finalize();
- *
- * MPI_Reduce(const void *sendbuf, void *recvbuf, int count,
- * MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm)
- *
- * MPI_DOUBLE
- * MPI_SUM
- *
- * MPI_Barrier(MPI_Comm comm)
- *
- */
 
 int procs;
 int rank;
@@ -68,7 +58,8 @@ load_locations (char *fn, double locs[][2], int first)
 /*
  * Sets the value for a lower and upper bound (lb, ub respectively) based on
  * a number of processes procs in which to divide the data with data_length in
- * an array, given a section_length and the number of processes and rank in the cluster.
+ * an array, given a section_length and the number of processes and rank in the
+ * cluster.
  */
 void
 get_bounds(int *lb, int *ub, int section_length, int data_length)
@@ -77,6 +68,22 @@ get_bounds(int *lb, int *ub, int section_length, int data_length)
   *ub = (rank >= (procs-1)) ? data_length : (rank+1) * section_length;
 }
 
+/*
+ * Distributes the calculation of coverage for the data points received.
+ * Each cluster iterates over a section of the data points array.
+ * The section is delimitted by lowerBoundary and upperBoundary, which
+ * are calculated by the routine get_bounds and passed by reference.
+ * After this, variables with the partial accumulated data to calculate
+ * coverage (hmsum, np_overall, nc) are "reduced" through by adding
+ * them altogether and writing the result in the following variables
+ * in the root node (rank=0):
+ * reduced_hmsum, reduced_np_overall, reduced_nc
+ *
+ * Finally these variables are used to calculate the coverage results
+ * and store them in the variables received by reference:
+ * result, npaths, ncoin.
+ *
+ */
 void
 coverage (double locs[][2], int nlocs, double *result, int *npaths, int *ncoin)
 {
@@ -84,48 +91,56 @@ coverage (double locs[][2], int nlocs, double *result, int *npaths, int *ncoin)
   double hmsum = 0.0, dsum, hm, d, y1, x1, y2, x2;
 
   int section_length = nlocs/procs;
-  int lb = 0, ub = 0; // lower and upper bound to calculate
-  get_bounds(&lb, &ub, section_length, nlocs);
+  int lowerBoundary = 0, upperBoundary = 0;
+  get_bounds(&lowerBoundary, &upperBoundary, section_length, nlocs);
 
-  // Calculate coverage of my section with the rest of the data
-  for (i = lb; i < ub; i++) {
+  for (i = lowerBoundary; i < upperBoundary; i++) {
     y1 = locs[i][0];
     x1 = locs[i][1];
     np = 0;
     dsum = 0.0;
     for (j = 0; j < nlocs; j++) {
+
       if (i != j) {
-	y2 = locs[j][0] - y1;
-	x2 = locs[j][1] - x1;
-	d = sqrt (y2 * y2 + x2 * x2);
-	if (d <= 0.0) {
-	  nc += 1;
-	} else {
-	  dsum += 1.0 / d;
-	  np += 1;
-	  np_overall += 1;
-	}
+
+        y2 = locs[j][0] - y1;
+        x2 = locs[j][1] - x1;
+        d = sqrt (y2 * y2 + x2 * x2);
+        if (d <= 0.0) {
+
+          nc += 1;
+
+        } else {
+
+          dsum += 1.0 / d;
+          np += 1;
+          np_overall += 1;
+
+        }
+
       }
     }
     hm = np / dsum;
     hmsum += 1.0 / hm;
+
   }
 
-  // Gather all the results
   int reduced_np_overall = 0, reduced_nc = 0;
   double reduced_hmsum = 0.0;
 
   /*
-   * Make sure all processes are synchronized, because MPI_Reduce, does not
+   * Make sure all processes are synchronized, because MPI_Reduce does not
    * guarantee synchronization.
    */
   MPI_Barrier(MPI_COMM_WORLD);
 
   MPI_Reduce(&hmsum,&reduced_hmsum,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
-  MPI_Reduce(&np_overall,&reduced_np_overall,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+
+  MPI_Reduce(&np_overall,&reduced_np_overall,1,
+      MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+
   MPI_Reduce(&nc,&reduced_nc,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
 
-  // Calculate the results based on the partial results of the cluster nodes.
   if (rank == 0) {
     *result = nlocs / reduced_hmsum;
     *npaths = reduced_np_overall;
@@ -238,9 +253,8 @@ mean_coverage4 (char *opname1, char *opname2, char *opname3, char *opname4,
 int
 main (int argc, char **argv)
 {
-  // Init MPI
+  // Init MPI and get information about the node in the cluster.
   MPI_Init(NULL, NULL);
-  // Get information about the cluster and node's rank in it.
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &procs);
 
@@ -277,7 +291,8 @@ main (int argc, char **argv)
         mc = mean_coverage3 (opnames[i1], opnames[i2], opnames[i3],
 			     imsets, nimsets);
         if (rank == 0) {
-          printf ("%s + %s + %s: %f\n", opnames[i1], opnames[i2], opnames[i3], mc);
+          printf ("%s + %s + %s: %f\n", opnames[i1], opnames[i2],
+              opnames[i3], mc);
         }
       }
     }
@@ -299,7 +314,7 @@ main (int argc, char **argv)
     }
   }
 
-  // Close MPI
+  // End MPI Connection
   MPI_Finalize();
 
   return EXIT_SUCCESS;
